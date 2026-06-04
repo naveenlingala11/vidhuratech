d9bbb18ba43dd91c067fb18afab417c1e0cddb7e
Naveen Kumar Lingala <naveenlingala11@gmail.com>
2026-06-04 21:55:16 +0530

    coding contest image changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://vidhuratech-nvlsp.ondigitalocean.app',
-  googleClientId: '286667410885-g7obhmbqbu6q01b5k675tjeh24piihle.apps.googleusercontent.com',
+  googleClientId: '520961987919-seo8c2uuc2o5c2bs41b3k792pnmoe88q.apps.googleusercontent.com',
   githubClientId: 'Ov23liFlkgLrx1gm7TeR',
 };
